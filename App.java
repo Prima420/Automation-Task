500612cf2990335767062918f7b5c2795656ff56
SHANZIDA BINTA AKRAM PRIMA <111388723+Prima420@users.noreply.github.com>
2023-06-07 18:16:32 +0600

    App.java

--- App.java
+++ App.java
@@ -16,135 +16,133 @@ public class App {
     }
     public void visitShareBus() {
         WebDriver driver;
-        // Visit the given URL
+        
         driver.get("https://test.sharebus.co/");
     }
 
     public void navigateToLoginPage() {
         By driver;
-        // Find and click the "Sign in" button
+       
         WebElement signInButton = driver.findElement((SearchContext) By.linkText("Sign in"));
         signInButton.click();
     }
 
     public void login(String email, String password) {
         By driver;
-        // Find the email and password fields and enter the provided credentials
+        
         WebElement emailField = driver.findElement((SearchContext) By.id("email"));
         emailField.sendKeys(email);
 
         WebElement passwordField = driver.findElement((SearchContext) By.id("password"));
         passwordField.sendKeys(password);
 
-        // Find and click the login button
+      
         WebElement loginButton = driver.findElement((SearchContext) By.xpath("//button[contains(text(),'Login')]"));
         loginButton.click();
     }
 
     public void selectUser(String user) {
         By driver;
-        // Find and click the user dropdown menu
+       
         WebElement userDropdown = driver.findElement((SearchContext) By.xpath("//div[@class='dropdown toggle_dropdown']"));
         userDropdown.click();
 
-        // Find and select the specified user from the options
+       
         WebElement userOption = driver.findElement((SearchContext) By.xpath("//a[contains(text(),'" + user + "')]"));
         userOption.click();
     }
 
     public void clickSetUpShareBus() {
         By driver;
-        // Find and click the "Set Up a ShareBus" button
+       
         WebElement setUpShareBusButton = driver.findElement((SearchContext) By.xpath("//button[contains(text(),'Set Up a ShareBus')]"));
         setUpShareBusButton.click();
     }
 
     public void insertTripDetails(String from, String to) {
         By driver;
-        // Find the "From" and "To" fields and enter the provided location addresses
+       
         WebElement fromField = driver.findElement((SearchContext) By.id("from"));
         fromField.sendKeys(from);
 
         WebElement toField = driver.findElement((SearchContext) By.id("to"));
         toField.sendKeys(to);
 
-        // Find and click the "Continue" button
+        
         WebElement continueButton = driver.findElement((SearchContext) By.xpath("//button[contains(text(),'Continue')]"));
         continueButton.click();
     }
 
     public void clickMembershipYes() {
         By driver;
-        // Find and click the "Yes" button on the membership page
+        
         WebElement yesButton = driver.findElement((SearchContext) By.xpath("//button[contains(text(),'Yes')]"));
         yesButton.click();
     }
 
     public void selectClub(String club) {
         By driver;
-        // Find and select the specified club from the options
+        
         WebElement clubOption = driver.findElement(By.xpath("//select[@id='club']//option[text()='" + club + "']"));
         clubOption.click();
     }
 
     public void clickMembershipContinue() {
         By driver;
-        // Find and click the "Continue" button on the membership page
+        
         WebElement continueButton = driver.findElement(By.xpath("//button[contains(text(),'Continue')]"));
         continueButton.click();
     }
 
     public void clickNoForTicket() {
         By driver;
-        // Find and click the "No" button for needing a ticket
+        
         WebElement noButton = driver.findElement((SearchContext) By.xpath("//button[contains(text(),'No')]"));
         noButton.click();
     }
 
     public void clickNoForTicketDiscounts() {
         By driver;
-        // Find and click the "No" button for activating ticket discounts
+       
         WebElement noButton = driver.findElement((SearchContext) By.xpath("//button[contains(text(),'No')]"));
         noButton.click();
     }
 
     public void clickCreateShareBus() {
         By driver;
-        // Find and click the "Create ShareBus" button
+       
         WebElement createShareBusButton = driver.findElement((SearchContext) By.xpath("//button[contains(text(),'Create ShareBus')]"));
         createShareBusButton.click();
     }
 
     public void clickPublish() {
         By driver;
-        // Find and click the "Publish" button
+       
         WebElement publishButton = driver.findElement((SearchContext) By.xpath("//button[contains(text(),'Publish')]"));
         publishButton.click();
     }
 
     public void insertDataOnRequiredFields() {
-        // Insert data on the required fields for the trip
-        // You can write code here to find and interact with the necessary elements
+        
     }
 
     public void clickReviewAndPublish() {
         By driver;
-        // Find and click the "Review and publish" button
+      
         WebElement reviewPublishButton = driver.findElement((SearchContext) By.xpath("//button[contains(text(),'Review and publish')]"));
         reviewPublishButton.click();
     }
 
     public void clickMyBuses() {
         By driver;
-        // Find and click the "My Busses" link in the navbar
+       
         WebElement myBusesLink = driver.findElement((SearchContext) By.xpath("//a[contains(text(),'My Busses')]"));
         myBusesLink.click();
     }
 
     public boolean isNewTripDisplayed() {
-        // Check if the new trip is displayed on the "My Busses" page
-        // You can write code here to find and verify the presence of the new trip element
-        return true; // Placeholder value, update accordingly
+       
+        return true;
     }
 
     public static void main(String[] args) {
